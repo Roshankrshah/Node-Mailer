e594f4f5a8d7058f66221ebac13ce4d9b1ed9bbf
Roshan <shahroshan0720@gmail.com>
2023-07-01 13:06:34 +0530

    setup controllers

---
+++ controllers/appController.js
@@ -0,0 +1,6 @@
+
+const signup = async (req,res)=>{
+    res.send("Hello Peter")
+}
+
+module.exports = signup;
--- routes/route.js
+++ routes/route.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const signup = require('../controllers/appController');
+const { sign } = require('crypto');
 
-router.get('/',(req,res)=>{
-    res.send("Hello Peter");
-});
+router.get('/',signup);
 
 module.exports = router;
