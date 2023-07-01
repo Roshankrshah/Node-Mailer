78ceabbcf2829d435c5258a0270e2bf157e31cfd
Roshan <shahroshan0720@gmail.com>
2023-07-01 13:57:07 +0530

    setup signup controller

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,6 +1,37 @@
+const nodemailer = require('nodemailer');
 
-const signup = async (req,res)=>{
-    res.send("Hello Peter")
+const signup = async (req, res) => {
+    let testAccount = await nodemailer.createTestAccount();
+
+    let transporter = nodemailer.createTransport({
+        host: "smtp.ethereal.email",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: testAccount.user, // generated ethereal user
+            pass: testAccount.pass, // generated ethereal password
+        },
+    });
+
+    let message = {
+        from: '"Fred Foo 👻" <foo@example.com>', // sender address
+        to: "bar@example.com, baz@example.com", // list of receivers
+        subject: "Hello ✔", // Subject line
+        text: "Successfully Register with us.", // plain text body
+        html: "<b>Successfully Register with us.</b>", // html body
+    }
+
+    transporter.sendMail(message).then((info)=>{
+        return res.status(201).json({
+            msg: `msg send successfully`,
+            info : info.messageId,
+            preview: nodemailer.getTestMessageUrl(info)
+        });
+    }).catch(error =>{
+        res.status(500).json({error});
+    })
 }
-
-module.exports = signup;
+const getbill = async (req, res) => {
+    res.status(201).json("Hello Spider");
+}
+module.exports = { signup, getbill };
--- routes/route.js
+++ routes/route.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const signup = require('../controllers/appController');
-const { sign } = require('crypto');
+const {signup, getbill} = require('../controllers/appController');
 
-router.get('/',signup);
+router.post('/user/signup',signup);
+router.post('/product/getbill',getbill);
 
 module.exports = router;
